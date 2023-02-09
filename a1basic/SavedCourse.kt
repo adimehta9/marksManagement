d9476e7e5a705515724a8b6cf7d3468510c643d5
adimehta9 <52846329+adimehta9@users.noreply.github.com>
2023-02-08 23:53:16 -0500

    Update SavedCourse.kt

--- a1basic/SavedCourse.kt
+++ a1basic/SavedCourse.kt
@@ -1,59 +1,126 @@
 package com.example.a1basic
 
-import javafx.beans.InvalidationListener
-import javafx.beans.Observable
+import javafx.event.EventHandler
 import javafx.geometry.Insets
-import javafx.geometry.Orientation
-import javafx.scene.control.Label
-import javafx.scene.control.Separator
+import javafx.geometry.Pos
+import javafx.scene.control.Button
+import javafx.scene.control.ChoiceBox
+import javafx.scene.control.TextField
+import javafx.scene.layout.Background
+import javafx.scene.layout.BackgroundFill
 import javafx.scene.layout.HBox
-import javafx.scene.text.Font
-import kotlin.math.round
-
-class StatusBarView(private val model: Model): HBox(), InvalidationListener {
-    override fun invalidated(observable: Observable?) {
-        var courseCount = 0
-        var gradeTotal = 0.0
-        var failedCount = 0
-        var numOfWDs = 0
+import javafx.scene.layout.Priority
+import javafx.scene.paint.Color
+import javafx.scene.shape.Rectangle
 
 
-        model.getCourses().forEach{
-            if(it.value.show) {
-                if (it.value.grade.uppercase() != "WD") {
-                    if (it.value.grade.toInt() < 50) {
-                        failedCount++
-                    }
-                    courseCount++
-                    gradeTotal += it.value.grade.toDouble()
-                } else {
-                    numOfWDs++
-                }
+// Display for each course within the Course list
+class SavedCourse(model: Model, id: String, name: String, t: String, g: String): HBox() {
+
+    // ID of created course (cannot be edited)
+    private val courseID: TextField = TextField().apply{
+        text = id
+        prefWidth = 80.0
+        prefHeight = 5.0
+        alignment = Pos.CENTER
+        isEditable = false
+    }
+
+    // name of the course
+     private val courseName: TextField = TextField().apply{
+        text = name
+        prefWidth = 300.0
+        prefHeight = 5.0
+        HBox.setHgrow(this, Priority.ALWAYS)
+        onKeyTyped = EventHandler {
+            // upon change
+            delOrUndo.text = "Undo"
+            update.isDisable = false
+        }
+    }
+
+    // term selected as choice box
+    private val term: ChoiceBox<String> = ChoiceBox<String>().apply{
+        value = t
+        items.addAll("F20", "W20", "S20", "F21", "W21", "S21", "F22", "W22", "S22", "F23", "W23", "S23")
+        onAction = EventHandler {
+            // upon change
+            update.isDisable = false
+            delOrUndo.text = "Undo"
+        }
+    }
+
+    // grade of the course
+    private val grade: TextField = TextField().apply {
+        text = g
+        prefWidth = 40.0
+        prefHeight = 5.0
+        onKeyTyped = EventHandler {
+            // upon change
+            update.isDisable = false
+            delOrUndo.text = "Undo"
+        }
+    }
+
+    // delete button or undo
+    private val delOrUndo: Button = Button("Delete").apply{
+        maxWidth = 70.5
+        prefHeight = 5.0
+
+
+        onAction = EventHandler {
+            if(text == "Delete") model.removeCourse(courseID.text)
+            else {
+                // must become undo -> revert values back
+                // make button delete again, and disable update button
+                courseName.text = model.getCourses()[courseID.text]?.name
+                term.value = model.getCourses()[courseID.text]?.term
+                grade.text = model.getCourses()[courseID.text]?.grade
+                text = "Delete"
+                update.isDisable = true
             }
         }
+    }
 
-        children.clear()
-        children.addAll(Label("Course Average: ${if(courseCount == 0) 
-            "n/a" else (round(gradeTotal/courseCount * 100.0)/100.0).toString()}").apply{font = Font.font(12.0) },
-            Separator().apply { orientation = Orientation.VERTICAL },
-            Label("Courses Taken: $courseCount").apply{font = Font.font(12.0)},
-            Separator().apply{orientation = Orientation.VERTICAL},
-            Label("Courses Failed: $failedCount").apply{font = Font.font(12.0)},
-        )
 
-        if(model.getCurWD()) {
-            children.addAll(Separator().apply { orientation = Orientation.VERTICAL },
-                Label("WD Courses: $numOfWDs").apply{font = Font.font(12.0) })
+    private val update: Button = Button("Update").apply{
+        maxWidth = 70.5
+        prefHeight = 5.0
+        isDisable = true
+
+        onAction = EventHandler {
+           try{
+               model.updateCourse(courseID.text, courseName.text, term.value, grade.text)
+               delOrUndo.text = "Delete"
+               isDisable = true
+           } catch(nfe:NumberFormatException) {
+               println("Invalid Input")
+           } catch (e: Exception) {
+               println(e.message)
+           }
         }
     }
 
 
-    init {
-        model.addListener(this)
-        invalidated(null)
-        padding = Insets(5.0, 5.0, 5.0, 15.0)
-        spacing = 10.0
-    }
 
+    private val c = if(g.uppercase() == "WD") Color.DARKSLATEGRAY
+    else if (g.toInt() < 50) Color.LIGHTCORAL
+    else if (g.toInt() < 60) Color.LIGHTBLUE
+    else if (g.toInt() < 91) Color.LIGHTGREEN
+    else if (g.toInt() < 96) Color.SILVER
+    else Color.GOLD
+
+
+    init{
+        children.addAll(courseID, courseName, term, grade, update, delOrUndo).apply {
+            background = Background(BackgroundFill(c, null, null))
+            spacing = 10.0
+            padding = Insets(10.0)
+            HBox.setHgrow(courseName, Priority.ALWAYS)
+        }
+
+
+
+    }
 
 }
